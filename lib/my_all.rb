d4f0ba05fb7dfcbfc45cba3fa2bd8ce29ce4f64f
Steve Womble <swomble@aol.com>
2020-04-23 16:09:47 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,11 @@ while i < collection.length
     block_return_values << yield(collection[i])
   i += 1 
   end
-  block_return_values
+  if block_return_values.include?(false)
+    false
+  else
+    true 
+  end
 end
 
 my_all?([1,2,3]) {|i| i < 2}
